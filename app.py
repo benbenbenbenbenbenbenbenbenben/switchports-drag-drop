df2f2924ea4737eac65c4195fb58f72b11818eda
Ben Kersnovske <ben.kersnovske@Bens-MacBook-Pro-50893.local>
2021-01-26 11:19:20 +0000

    “update”

--- app.py
+++ app.py
@@ -116,12 +116,11 @@ def get_switch(serial):
 #----------------------------------------------------------------------------#
 
 # Debug:
-if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5001)
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-'''
+
