e7d68d32e10d65791cd35bedaed8739a3751fbdf
Ben Kersnovske <ben.kersnovske@Bens-MacBook-Pro-50893.local>
2021-01-26 10:28:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Meraki Switchport Drag & Drop 
+
+Web App to easily configure switchports using premade port profiles. This uses Python Flask for backend with simple JSON file to store profiles. Front End uses native JS for drag and drop functionality and Bootstrap for styling.  
+
+## Table of Contents
+- [Examples](#examples)
+- [Setup](#setup)
+- [Profile Example JSON](#profile-example-json)
+
+
+## Examples
+- [8 Port Switch](/QBSB-S48U-VH7E)
+- [24 Port Switch](/Q2EW-ATJ2-QFN3)
+- [48 Port Switch](/QBSB-AU53-GZLN)
+
+![Image of App]()
+
+![Gif of App]()
+
+## Setup
+1. Ensure you have Python version 3 installed (If not download and install)
+```bash
+python3 -V
+```
+2. Create a Python 3 virtual environment
+```bash
+python3 -m venv meraki-venv
+cd meraki-venv/
+source bin/activate
+```
+3. Clone Github Repository
+```bash
+git clone https://github.com/benbenbenbenbenbenbenbenbenben/switchports-drag-drop.git
+cd switchports-drag-drop/
+```
+4. Install project requirements into the virtual environment
+```bash
+pip install -r requirements.txt
+```
+5. Add Your Meraki API Key To Env Variables
+```bash
+export MERAKI_DASHBOARD_API_KEY=YOUR_API_KEY
+```
+6. Run the App
+```bash
+python app.py
+```
+
+## Profile Example JSON
+Required keys for app are name, icon, hex. All other params are optional for port config- https://developer.cisco.com/meraki/api-v1/#!update-device-switch-port
+```json
+[
+    {
+        "name": "Wifi",
+        "tags": ["wifi","profile"],
+        "enabled": true,
+        "poeEnabled": true,
+        "type": "trunk",
+        "vlan": "2",
+        "icon": "wifi",
+        "hex": "#57cc99"
+    }
+]
+```
---
+++ app.py
@@ -0,0 +1,127 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, request, render_template
+import meraki
+import json
+
+#----------------------------------------------------------------------------#
+# App Config
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+app_title = 'Switch Drag & Drop'
+# Instantiate a Meraki dashboard API session
+m = meraki.DashboardAPI(
+    api_key='',
+    base_url='https://api-mp.meraki.com/api/v1/',
+    output_log=False,
+    # log_file_prefix=os.path.basename(__file__)[:-3],
+    # log_path='',
+    print_console=False
+)
+
+#----------------------------------------------------------------------------#
+# Controllers
+#----------------------------------------------------------------------------#
+
+# Main Page
+@app.route('/<serial>', methods=["GET"])
+def switch(serial):
+    try:
+        switch = get_switch(serial)
+    except:
+        return render_template('base.html', app_title=app_title, contents='404.html', serial=serial)
+    ports = get_ports(serial)
+    profiles = read_json_file('profiles')
+    return render_template('base.html', app_title=app_title, contents='switch.html', profiles=profiles, ports=ports, serial=serial, switch=switch)
+
+# Ports
+@app.route('/ports', methods=["PUT"])
+def ports():
+    # Update Port
+    if request.method == "PUT":
+        # URL Params
+        serial = request.args.get('serial')
+        port = request.args.get('port')
+        profileName = request.args.get('profile')
+        # Get Port Profiles
+        profilesJSON = read_json_file('profiles')
+        profile = get_profile(profilesJSON, profileName)
+        if profile["name"].lower() == "reset":
+            profile["name"] = ""
+        try:
+            updatePort = m.switch.updateDeviceSwitchPort(serial, port, **profile)
+            return json.dumps(updatePort)
+        except meraki.APIError as e:
+            print(f'Meraki API error: {e}')
+            print(f'status code = {e.status}')
+            print(f'reason = {e.reason}')
+            print(f'error = {e.message}')
+            return f'reason = {e.reason}'
+        except Exception as e:
+            print(f'some other error: {e}')
+            return f'reason = {e}'
+
+#----------------------------------------------------------------------------#
+# Functions
+#----------------------------------------------------------------------------#
+
+# Read JSON File
+def read_json_file(file):
+    try:
+        with open(f'static/{file}.json') as f:
+            return json.load(f)
+    except OSError as e: 
+        print(e)
+        return False
+
+# Get Profile
+def get_profile(profiles, profile):
+    for p in profiles:
+        if p["name"].lower() == profile.lower():
+            port_params = ['name', 'tags', 'enabled', 'type', 'vlan', 'voiceVlan', 'allowedVlans', 'poeEnabled', 'isolationEnabled', 'rstpEnabled', 'stpGuard', 'linkNegotiation', 'portScheduleId', 'udld', 'accessPolicyType', 'accessPolicyNumber', 'macAllowList', 'stickyMacAllowList', 'stickyMacAllowListLimit', 'stormControlEnabled', 'flexibleStackingEnabled', ]
+            payload = {k.strip(): v for k, v in p.items() if k.strip() in port_params}
+            return payload
+
+# Get Ports
+def get_ports(serial):
+    try:
+        ports = m.switch.getDeviceSwitchPorts(serial)
+    except meraki.APIError as e:
+        print(f'Meraki API error: {e}')
+        print(f'status code = {e.status}')
+        print(f'reason = {e.reason}')
+        print(f'error = {e.message}')
+    except Exception as e:
+        print(f'some other error: {e}')
+    return ports
+
+# Get Switch
+def get_switch(serial):
+    try:
+        switch = m.devices.getDevice(serial)
+    except meraki.APIError as e:
+        print(f'Meraki API error: {e}')
+        print(f'status code = {e.status}')
+        print(f'reason = {e.reason}')
+        print(f'error = {e.message}')
+    except Exception as e:
+        print(f'some other error: {e}')
+    return switch
+
+#----------------------------------------------------------------------------#
+# Launch
+#----------------------------------------------------------------------------#
+
+# Debug:
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+# Or specify port manually:
+'''
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+'''
