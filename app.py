138a3d05d9f1892292f7117a62fb4fcec2df6ed3
Ben Kersnovske <ben.kersnovske@Bens-MacBook-Pro-50893.local>
2022-07-26 16:25:13 +0100

    added feature to paint ports

--- app.py
+++ app.py
@@ -150,11 +150,11 @@ def get_switch(serial):
 #----------------------------------------------------------------------------#
 
 # Debug:
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    app.run(debug=True, port=5050)
 
 # Or specify port manually:
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5050))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5050))
+#     app.run(host='0.0.0.0', port=port)
 
